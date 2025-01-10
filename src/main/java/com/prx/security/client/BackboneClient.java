f26a7f87cdde3d41da76e37686daf5322c30349d
Luis Mata <69470188+lanmata@users.noreply.github.com>
2025-01-10 14:36:21 -0500

    DS-64 Create Authentication Classes for User Authentication
    Updated pom.xml file

--- src/main/java/com/prx/security/client/BackboneClient.java
+++ src/main/java/com/prx/security/client/BackboneClient.java
@@ -5,9 +5,13 @@ import com.prx.security.client.to.BackboneUserCreateResponse;
 import com.prx.security.interceptor.BackendFeignClientInterceptor;
 import com.prx.security.to.AuthRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.UUID;
 
 import static com.prx.security.constant.ConstantApp.SESSION_TOKEN_KEY;
 
@@ -16,6 +20,12 @@ public interface BackboneClient {
 
     @GetMapping("/api/v1/session/validate")
     boolean validate(@RequestHeader(SESSION_TOKEN_KEY) String sessionToken);
+
+    @GetMapping("/api/v1/users/check/alias/{alias}/application/{applicationId}")
+    ResponseEntity<Void> checkAlias(@PathVariable String alias, @PathVariable UUID applicationId);
+
+    @GetMapping("/api/v1/users/check/email/{email}/application/{applicationId}")
+    ResponseEntity<Void> checkEmail(@PathVariable String email, @PathVariable UUID applicationId);
 
     @PostMapping("/api/v1/session")
     String token(AuthRequest authRequest);
